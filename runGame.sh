1109649e2e48bec914f13e993d5d70df726def12
nox-x <christoph.ruhl@protonmail.ch>
2016-06-11 22:11:24 +0200

    fixed touchscreen offset

--- runGame.sh
+++ runGame.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axis Calibration" "1701 1641 641 638"
-xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 0 1.15 0 1.15 0 0 0 0 1
+xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axes Swap" "1"
+xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 1.3 0 -0.15 0 1.3 -0.15 0 0 1
 cd /home/pi/drinks-scanner-display && python game.py
