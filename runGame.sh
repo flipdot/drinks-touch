8a8f25f1cf1a8af9a58fd1d4d2da06b1fa68df58
Your Name <you@example.com>
2016-07-24 23:25:58 +0200

    added while loop to resart app in case it crashes

--- runGame.sh
+++ runGame.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axes Swap" "1"
 xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 1.3 0 -0.15 0 1.3 -0.15 0 0 1
-cd /home/pi/drinks-scanner-display && ENV=PI python game.py
+
+while [ 1 ]:
+do
+    cd /home/pi/drinks-scanner-display && ENV=PI python game.py
+done
