d90f70fe74d12dc5995eb6c87373409c3b9f3678
Your Name <you@example.com>
2016-07-24 23:27:59 +0200

    fixed typo

--- runGame.sh
+++ runGame.sh
@@ -2,7 +2,7 @@
 xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axes Swap" "1"
 xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 1.3 0 -0.15 0 1.3 -0.15 0 0 1
 
-while [ 1 ]:
+while [ 1 ];
 do
     cd /home/pi/drinks-scanner-display && ENV=PI python game.py
 done
