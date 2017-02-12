0bd9d285948ccb67d0f257b33d6db819678fe7bd
Claus Strasburger <claus@strasburger.de>
2017-02-12 20:32:06 +0100

    Webserver: nicer layout and allow debugging

--- game.py
+++ game.py
@@ -3,8 +3,10 @@
 import pygame
 import logging
 import threading
+import subprocess
 import Queue
 import traceback
+import sys
 
 logging.basicConfig(level=logging.DEBUG)
 
@@ -19,33 +21,8 @@ from webserver.webserver import run as run_webserver
 from barcode.barcode_reader import run as run_barcode_reader
 from barcode.barcode_worker import Worker as BarcodeWorker
 
-screen = get_screen()
-clock = pygame.time.Clock()
-
-drinks_manager = DrinksManager()
-DrinksManager.set_instance(drinks_manager)
-
-screen_manager = ScreenManager(screen)
-ScreenManager.set_instance(screen_manager)
-
-init_db()
-
-##### Barcode Scanner #####
-
-barcode_worker = BarcodeWorker()
-barcode_thread = threading.Thread(
-    target=run_barcode_reader,
-    args=(barcode_worker,)
-)
-barcode_thread.daemon = True
-barcode_thread.start()
-
-##### webserver #####
-web_thread = threading.Thread(
-    target=run_webserver
-)
-web_thread.daemon = True
-web_thread.start()
+event_queue = Queue.Queue()
+screen_manager = None
 
 #### Events ####
 def handle_events():
@@ -64,34 +41,68 @@ def handle_events():
             traceback.print_exc()
             pass
 
-event_queue = Queue.Queue()
-event_thread = threading.Thread(
-    target=handle_events
-)
-event_thread.daemon = True
-event_thread.start()
-
 ##### Rendering #####
+def main(argv):
+    if "--webserver" in argv:
+        run_webserver()
+        return 0
 
-t = 0
-dt = 0
-done = False
-while not done:
-    dt = clock.tick(30) / 1000.0
-    t += dt
+    screen = get_screen()
+    clock = pygame.time.Clock()
 
-    current_screen = screen_manager.get_active()
+    drinks_manager = DrinksManager()
+    DrinksManager.set_instance(drinks_manager)
 
-    screen.fill((0, 0, 0))
-    current_screen.render(t, dt)
+    global screen_manager
+    screen_manager = ScreenManager(screen)
+    ScreenManager.set_instance(screen_manager)
 
-    pygame.display.flip()
-    
-    events = pygame.event.get()
-    for e in events:
-        e.t = t
-        e.dt = dt
-        if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
-            done = True
-            break
-        event_queue.put(e, True)
+    init_db()
+
+    ##### Barcode Scanner #####
+    barcode_worker = BarcodeWorker()
+    barcode_thread = threading.Thread(
+        target=run_barcode_reader,
+        args=(barcode_worker,)
+    )
+    barcode_thread.daemon = True
+    barcode_thread.start()
+
+    ##### webserver needs to be a main thread #####
+    web_thread = subprocess.Popen([sys.argv[0], "--webserver"])
+
+    event_thread = threading.Thread(
+        target=handle_events
+    )
+    event_thread.daemon = True
+    event_thread.start()
+
+
+    t = 0
+    dt = 0
+    done = False
+    while not done:
+        dt = clock.tick(30) / 1000.0
+        t += dt
+
+        current_screen = screen_manager.get_active()
+
+        screen.fill((0, 0, 0))
+        current_screen.render(t, dt)
+
+        pygame.display.flip()
+        
+        events = pygame.event.get()
+        for e in events:
+            e.t = t
+            e.dt = dt
+            if e.type == QUIT or (e.type == KEYDOWN and e.key == K_ESCAPE):
+                done = True
+                break
+            event_queue.put(e, True)
+    web_thread.terminate()
+    web_thread.wait()
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv))
--- webserver/webserver.py
+++ webserver/webserver.py
@@ -1,4 +1,6 @@
 import datetime
+import requests
+import re
 
 from env import is_pi
 
@@ -11,48 +13,59 @@ from database.storage import get_session
 from database.storage import init_db
 from database.models.recharge_event import RechargeEvent
 
-
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+uid_pattern = re.compile("^\d+$")
 
+@app.route('/')
 @app.route('/recharge')
-def recharge():
+def index():
     users = Users.get_all()
     users.insert(0,{})
-    return render_template('recharge.html', users=users)
+    return render_template('index.html', users=users)
 
 @app.route('/recharge/doit', methods=['POST'])
 def recharge_doit():
-    user = request.form['user_user']
-    helper = request.form['helper_user']
+    user_id = request.form['user_user']
+    helper_id = request.form['helper_user']
     amount = request.form['amount']
-
-    if not user or not helper or amount <= 0:
+    if not user_id or not helper_id or amount <= 0:
         return 'Please enter valid data!'
 
+    if not uid_pattern.match(user_id):
+        return "Invalid user id"
+    if not uid_pattern.match(helper_id):
+        return "Invalid helper id"
+    
+    users = Users.get_all(filters=['uidNumber='+user_id])
+    helpers = Users.get_all(filters=['uidNumber='+helper_id])
+
+    if not users:
+        return "user %s not found" % user_id
+    if not helpers:
+        return "user %s not found" % helper_id
+    user = users[0]
+    helper = helpers[0]
+    
     session = get_session()
     ev = RechargeEvent(
-        user,
-        helper,
+        user['id'],
+        helper['id'],
         amount
     )
     
     session.add(ev)
     session.commit()
 
-    return render_template('recharge_success.html', amount=amount)
-
+    return render_template('recharge_success.html', amount=amount, user=user)
 
 def run():
     port = 5000
-
     if is_pi():
         port = 80
 
     app.run(
         host='0.0.0.0',
-        port=port
+        port=port,
+        debug=not is_pi()
     )
