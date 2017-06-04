3c00cfc02f0de0fc3107718c7b51b6398dbedc07
Daniel Huhn <daniel@danielhuhn.de>
2017-06-04 22:54:39 +0200

    added script to import ldap users into db for simple data aggregation

---
+++ database/models/ldapUser.py
@@ -0,0 +1,18 @@
+from sqlalchemy import Column, Integer, String, DateTime, Numeric, Boolean
+from database.storage import Base
+
+class LdapUser(Base):
+    __tablename__ = 'ldapUsers'
+    id = Column(Integer, primary_key=True)
+    ldapId = Column(String(20), unique=False)
+    name = Column(String(50), unique=False)
+    path = Column(String(50), unique=False)
+    id_card = Column(String(50), unique=False)
+    is_card = Column(Boolean(), unique=False)
+
+    def __init__(self, ldapId, name, path, id_card, is_card):
+        self.ldapId = ldapId
+        self.name = name
+        self.path = path
+        self.id_card = id_card
+        self.is_card = is_card
