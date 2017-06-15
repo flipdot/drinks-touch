0b756577d2a3c7d95354250be5efea724d4baa83
Daniel Huhn <daniel@danielhuhn.de>
2017-06-16 00:08:04 +0200

    added clever replacement for wrong chars

--- barcode/barcode_reader.py
+++ barcode/barcode_reader.py
@@ -11,6 +11,39 @@ def run(worker):
         while True:
             worker.on_barcode(sys.stdin.readline().strip())
 
+    def replace_key_code(barcode, replacements):
+
+        indexes_per_replacement = {}
+
+        for source in replacements:
+            indexes = []
+
+            i = 0
+            while True:
+                try:
+                    pos = barcode.index(source, i)
+                    indexes.append(pos)
+                    i = i + 1
+                except ValueError:
+                    break
+
+            indexes_per_replacement[source] = indexes
+
+        for source in indexes_per_replacement:
+            indexes = indexes_per_replacement[source]
+            target = replacements[source]
+
+            for index in indexes:
+                barcode = barcode[:index] + target + barcode[index + 1:]
+
+        return barcode
+
     while True:
         for barcode in keyboard.get_typed_strings(keyboard.record(until="tab")):
+
+            barcode = replace_key_code(replace_key_code, {
+                "?": "_",
+                "_": "?"
+            })
+
             worker.on_barcode(barcode)
