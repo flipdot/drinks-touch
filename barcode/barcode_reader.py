6f81b8ce0ee764735d38066b5d0d5e515e3f6318
Daniel Huhn <daniel@danielhuhn.de>
2017-06-15 21:51:31 +0200

    switched from c-program to keyboard package for barcode reading

--- barcode/barcode_reader.py
+++ barcode/barcode_reader.py
@@ -7,12 +7,9 @@ import threading
 
 import pprint
 
-from env import is_pi
+import keyboard
 
-command = ['barcode_bin/barcode', '/dev/input/by-id/usb-GASIA_PS2toUSB_Adapter-event-kbd']
-#command = ['barcode_bin/barcode', '/dev/input/by-id/usb-Logitech_USB_Receiver-if02-event-kbd']
-devnull = open(os.devnull, 'wb')
-last_barcode = time.time()
+from env import is_pi
 
 def run(worker):
     global last_barcode
@@ -21,36 +18,19 @@ def run(worker):
         print "Enter EAN here to simulate scanned barcode!"
         while True:
             worker.on_barcode(sys.stdin.readline().strip())
-    while True:
-        #print "starting", command
-        process = subprocess.Popen(
-            command,
-            shell=False,
-            stdout=subprocess.PIPE,
-            stderr=devnull
-        )
-        last_barcode = time.time()
-        reaper = threading.Thread(
-            target=reap, args=(process,))
-        reaper.start()
-        buffer = ''
-        for c in iter(lambda: process.stdout.read(1), ''):
-            if c == '\n':
-                last_barcode = time.time()
-                print "barcode: ", buffer
-                worker.on_barcode(buffer)
-                buffer = ''
-            else:
-                buffer += c
-        process.communicate()
-        reaper.join()
 
-def reap(process):
-    global last_barcode
-    while process.poll() == None:
-        if abs(last_barcode - time.time()) > 30:
-            #print "restarting barcode process"
-            process.kill()
-            break
+    global keyboard_buffer
+    keyboard_buffer = ''
+
+    def keyboard_up_callback(event):
+        global keyboard_buffer
+
+        c = event.name
+        if c == 'tab':
+            print "barcode: ", keyboard_buffer
+            worker.on_barcode(keyboard_buffer)
+            keyboard_buffer = ''
         else:
-            time.sleep(0.5)
+            keyboard_buffer += c
+
+    keyboard.on_release(keyboard_up_callback)
