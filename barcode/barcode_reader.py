2e6a9d054d543990472c4c9add7801552d313676
Daniel Huhn <daniel@danielhuhn.de>
2017-06-15 23:51:31 +0200

    trying new barcode reading mode

--- barcode/barcode_reader.py
+++ barcode/barcode_reader.py
@@ -1,12 +1,4 @@
-import io
-import subprocess
 import sys
-import os
-import time
-import threading
-
-import pprint
-
 import keyboard
 
 from env import is_pi
@@ -19,31 +11,6 @@ def run(worker):
         while True:
             worker.on_barcode(sys.stdin.readline().strip())
 
-    global keyboard_buffer
-    keyboard_buffer = ''
-
-    def apply_code_fix(char):
-        if char == '?':
-            return '_'
-
-        if char == '_':
-            return '?'
-
-        return char
-
-    def keyboard_up_callback(event):
-        global keyboard_buffer
-
-        c = apply_code_fix(event.name)
-        if c == 'tab':
-            print "barcode: ", keyboard_buffer
-            worker.on_barcode(keyboard_buffer)
-            keyboard_buffer = ''
-        elif c == 'shift':
-            # we may ignore the shift
-            # at the beginning of a barcode
-            pass
-        else:
-            keyboard_buffer += c
-
-    keyboard.on_release(keyboard_up_callback)
+    while True:
+        for barcode in keyboard.get_typed_strings(keyboard.record()):
+            worker.on_barcode(barcode)
