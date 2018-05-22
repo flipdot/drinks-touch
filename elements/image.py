0799c59f9f3fa206f668711f9ddd4379901f9ccb
Claus Strasburger <claus@strasburger.de>
2018-05-22 21:42:34 +0200

    images: support rendering with resizing

--- elements/image.py
+++ elements/image.py
@@ -2,14 +2,18 @@ import pygame
 
 from .base_elm import BaseElm
 
+
 class Image(BaseElm):
     def __init__(self, screen, **kwargs):
         self.pos = kwargs.get('pos', (0, 0))
         self.src = kwargs.get('src', 'img/test.jpg')
+        self.size = kwargs.get('size', None)
 
         self.__load_img()
 
         pos = kwargs.get('pos', (0, 0))
+        if self.size:
+            self.img = pygame.transform.smoothscale(self.img, self.size)
         img_size = self.img.get_size()
         super(Image, self).__init__(screen, pos, img_size[1], img_size[0])
 
