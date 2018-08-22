e60bf45ac47a370ad416ad2f4f67e88707d83672
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-08-23 00:09:26 +0200

    How Much Is The Fish

--- screens/new_id_screen.py
+++ screens/new_id_screen.py
@@ -48,7 +48,7 @@ class NewIDScreen(Screen):
         ))
         self.objects.append(Label(
             self.screen,
-            text = "wieviel hast du reingeworfen?",
+            text = "Wie viel hast du eingeworfen?",
             pos=(30, 220),
             size=40
         ))
