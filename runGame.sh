fca29e992a21950a2bff580442d6045735aa5565
Claus Strasburger <claus@strasburger.de>
2018-09-04 22:07:26 +0200

    env variables

--- runGame.sh
+++ runGame.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "Starting in 10 seconds!"
-sleep 10
+echo "Starting in 5 seconds!"
+sleep 5
 function fix_display () {
     xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axes Swap" "1"
     xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 1.3 0 -0.15 0 1.3 -0.15 0 0 1
@@ -16,7 +16,10 @@ while [ 1 ];
 do
     (
         set -e
+        export ENV=PI
+        export PYTHONUNBUFFERED=1
+
         cd /home/pi/drinks-scanner-display
-        ENV=PI systemd-cat -t"drinks" ./game.py
+        systemd-cat -t"drinks" ./game.py
     )
 done
