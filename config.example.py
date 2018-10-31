93a47f1c08b04be391b3df24f39a97d9900e3545
Jonas Thelemann <e-mail@jonas-thelemann.de>
2018-10-31 20:38:29 +0100

    Postgres Connection String
    + Configuration option "POSTGRES_CONNECTION_STRING"

--- config.example.py
+++ config.example.py
@@ -5,6 +5,8 @@ money_password = "my_basicauth_passwd"
 LDAP_HOST = "ldaps://ldap.example.com"
 LDAP_PW = ""
 
+POSTGRES_CONNECTION_STRING = "postgresql://postgres:postgres@127.0.0.1/drinks"
+
 MAIL_FROM = "flipdot-noti@vega.uberspace.de"
 MAIL_PW = "pw"
 MAIL_HOST = "vega.uberspace.de"
