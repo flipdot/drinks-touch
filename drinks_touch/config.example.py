90e7b7eaade47146871ca50657fb6e8e8b8b767c
Jonas Thelemann <e-mail@jonas-thelemann.de>
2019-04-21 22:04:57 +0200

    Configuration: Add PostgreSQL Connection String Alternative

--- drinks_touch/config.example.py
+++ drinks_touch/config.example.py
@@ -6,6 +6,7 @@ LDAP_HOST = "ldaps://ldap.example.com"
 LDAP_PW = ""
 
 POSTGRES_CONNECTION_STRING = "postgresql://postgres:postgres@127.0.0.1/drinks"
+# POSTGRES_CONNECTION_STRING = "postgresql://postgres:postgres@postgres/drinks" # Docker
 
 MAIL_FROM = "flipdot-noti@vega.uberspace.de"
 MAIL_PW = "pw"
