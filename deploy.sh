cbae879e671e6d822eec080e768c4e01339b4bad
malled2002 <malte@drabesch.de>
2024-02-11 14:08:34 +0100

    add deploy script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
+
+rsync -rv --exclude drinks_touch/config.py --exclude .git --exclude myenv --exclude __pycache__ $SCRIPT_DIR pi@drinks.flipdot.space:/home/pi/drinks-touch/
