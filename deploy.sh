10089558defff5ca0fa5d53ddf4a93566bd65886
Malte <malte@drabesch.de>
2024-02-11 16:05:06 +0100

    rotate display in X11

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 
-rsync -rv --exclude drinks_touch/config.py --exclude .git --exclude myenv --exclude __pycache__ $SCRIPT_DIR pi@drinks.flipdot.space:/home/pi/drinks-touch/
+rsync -rv --exclude drinks_touch/config.py --exclude .git --exclude myenv --exclude __pycache__ $SCRIPT_DIR/ pi@drinks.flipdot.space:/home/pi/drinks-touch/
--- runGame.sh
+++ runGame.sh
@@ -3,6 +3,7 @@
 echo "Starting in 5 seconds!"
 sleep 5
 function fix_display () {
+    xrandr --output HDMI-1 --rotate left
     xinput set-prop "eGalax Inc. USB TouchController" "Evdev Axes Swap" "1"
     xinput set-prop "eGalax Inc. USB TouchController" "Coordinate Transformation Matrix" 1.3 0 -0.15 0 1.3 -0.15 0 0 1
 }
