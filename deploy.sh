f63513eb98ebe57edc3853a8ee785821ca445c15
Claus F. Strasburger <1156864+cfstras@users.noreply.github.com>
2024-02-13 21:31:46 +0100

    Fix deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+set -euo pipefail
 
+HOST=drinks.flipdot.space
+USER=root
+DIR=/opt/drinks-touch
 
-rsync -rv --exclude drinks_touch/config.py --exclude .git --exclude myenv --exclude __pycache__ $SCRIPT_DIR/ pi@drinks.flipdot.space:/home/pi/drinks-touch/
+CONN="${USER}@${HOST}"
+
+rsync -rlptv --exclude drinks_touch/config.py --exclude .git --exclude myenv --exclude __pycache__ \
+    "${SCRIPT_DIR}/" "${CONN}:${DIR}/"
+
+#shellcheck disable=SC2029
+ssh "${CONN}" "cd ${DIR} &&  ./setup_all_docker.sh"
+
+# For future shenanigans
+# echo "Please commit first."
+# git remote remove pi || :
+# git remote add pi root@drinks-touch.flipdot.space:/opt/drinks-touch/.git
+# git push pi:from_remote
+# ssh root@drinks-touch.flipdot.space "cd /opt/drinks-touch && git merge --ff  && ./setup_all_docker.sh"
--- setup_all_docker.sh
+++ setup_all_docker.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 set -euo pipefail
+set -x
+
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 cd "${SCRIPT_DIR}"
 
-if [[ "$UID" != 0 ]];
+if [[ "$UID" != 0 ]]; then
   echo "Please run me as root."
   exit 1
-fi	
+fi
 
 cp docker/drinks_touch_xorg.service /etc/systemd/system/
 cp docker/xinitrc $HOME
@@ -15,4 +17,5 @@ cp docker/xinitrc $HOME
 systemctl daemon-reload
 systemctl enable --now drinks_touch_xorg.service
 
-docker compose -f compose.prod.yaml up -d
+docker compose -f compose.prod.yaml down
+docker compose -f compose.prod.yaml up -d --build
