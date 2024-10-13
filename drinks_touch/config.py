8ed5aeec7a4f5dbcd12e7c10997e6cf585b5f121
Sören Wegener <wegener92@gmail.com>
2024-10-13 23:44:19 +0200

    Add build number (#225)
    Determine at runtime via git, if not part of the build-process.
    
    @dargmuesli any idea how we can inject the build number from the build
    process in the docker image? There is a `with: TAG: ...` in the ci.yml
    which uses your github action, can and should we access that tag within
    the Dockerfile?

--- drinks_touch/config.py
+++ drinks_touch/config.py
@@ -9,6 +9,13 @@ LDAP_HOST = os.environ.get("LDAP_HOST", "ldap://127.0.0.1")
 LDAP_USER = os.environ.get("LDAP_USER", "cn=admin,dc=flipdot,dc=org")
 LDAP_PW = os.environ.get("LDAP_PW", "admin")
 
+if bn := os.environ.get("BUILD_NUMBER"):
+    BUILD_NUMBER = bn
+else:
+    BUILD_NUMBER = (
+        os.popen("git describe --tags --dirty").read().strip() or "git is not available"
+    )
+
 
 OIDC_DISCOVERY_URL = os.environ.get(
     "OIDC_DISCOVERY_URL",
