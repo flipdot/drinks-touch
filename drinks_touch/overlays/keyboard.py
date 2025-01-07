1698802a756ec410a0612a1cb03018826ca478f5
Soeren Wegener <wegener92@gmail.com>
2025-01-07 22:50:56 +0100

    Implement numkeypad

--- drinks_touch/overlays/keyboard.py
+++ drinks_touch/overlays/keyboard.py
@@ -1,10 +1,13 @@
 import enum
+import functools
 
 import pygame
 from pygame.event import EventType
 
-from elements import Label
+from elements import Label, Button
 from elements.base_elm import BaseElm
+from elements.hbox import HBox
+from elements.vbox import VBox
 from overlays import BaseOverlay
 from screens.screen_manager import ScreenManager
 
@@ -27,25 +30,104 @@ class KeyboardOverlay(BaseOverlay):
             - self.height
             - self.screen_manager.MENU_BAR_HEIGHT,
         )
+        self._layout_override = None
 
         self._anim_start_pos = pygame.Vector2(
             0, self.screen.get_height() - self.height * 0.9
         )
+
+        num_btn_size = 50
+
+        def make_num_btn(n: str):
+            return Button(
+                text=f" {n} ",
+                size=num_btn_size,
+                on_click=functools.partial(
+                    self.press_key, getattr(pygame, f"K_{n}"), n
+                ),
+            )
+
         self.layouts: dict[KeyboardLayout, list[BaseElm]] = {
             KeyboardLayout.DEFAULT: [
                 Label(
                     text="Keyboard (default layout)",
                     pos=(0, 0),
-                )
+                ),
+                Button(
+                    text="123",
+                    pos=(0, self.height - 75),
+                    size=40,
+                    on_click=functools.partial(self.set_layout, KeyboardLayout.NUMERIC),
+                ),
             ],
             KeyboardLayout.NUMERIC: [
-                Label(
-                    text="Keyboard (numeric layout)",
+                VBox(
+                    [
+                        HBox(
+                            [
+                                make_num_btn("1"),
+                                make_num_btn("2"),
+                                make_num_btn("3"),
+                                Button(
+                                    text=" ← ",
+                                    size=num_btn_size,
+                                    on_click=functools.partial(
+                                        self.press_key, pygame.K_BACKSPACE
+                                    ),
+                                ),
+                            ]
+                        ),
+                        HBox(
+                            [
+                                make_num_btn("4"),
+                                make_num_btn("5"),
+                                make_num_btn("6"),
+                            ]
+                        ),
+                        HBox(
+                            [
+                                make_num_btn("7"),
+                                make_num_btn("8"),
+                                make_num_btn("9"),
+                            ]
+                        ),
+                        HBox(
+                            [
+                                Button(
+                                    text=" - ",
+                                    size=num_btn_size,
+                                    on_click=functools.partial(
+                                        self.press_key, pygame.K_MINUS, "-"
+                                    ),
+                                ),
+                                make_num_btn("0"),
+                                Button(
+                                    text=" . ",
+                                    size=num_btn_size,
+                                    on_click=functools.partial(
+                                        self.press_key, pygame.K_PERIOD, "."
+                                    ),
+                                ),
+                                Button(
+                                    text="abc",
+                                    size=num_btn_size,
+                                    on_click=functools.partial(
+                                        self.set_layout, KeyboardLayout.DEFAULT
+                                    ),
+                                ),
+                            ]
+                        ),
+                    ],
                     pos=(0, 0),
-                )
+                    padding=10,
+                ),
             ],
         }
         self.clock = 0
+
+    def press_key(self, key: int, char: str | None = None):
+        event = pygame.event.Event(pygame.KEYDOWN, key=key, unicode=char)
+        self.screen_manager.events([event])
 
     def render(self, dt):
         if not self.visible:
@@ -83,6 +165,7 @@ class KeyboardOverlay(BaseOverlay):
 
     def events(self, events: list[EventType]):
         if not self.visible:
+            self.set_layout(None)
             return False
         # check if click is inside keyboard
         for event in events:
@@ -97,8 +180,8 @@ class KeyboardOverlay(BaseOverlay):
                 if self.collides_with(transformed_pos):
                     event.dict["consumed"] = True
                     for obj in self.layout:
-                        if consumed_by := obj.event(event, transformed_pos):
-                            return consumed_by
+                        if obj.event(event, transformed_pos):
+                            continue
 
     def collides_with(self, pos: tuple[float, float]) -> bool:
         return pygame.Rect(0, 0, self.width, self.height).collidepoint(pos[0], pos[1])
@@ -114,4 +197,9 @@ class KeyboardOverlay(BaseOverlay):
 
     @property
     def layout(self):
+        if self._layout_override:
+            return self.layouts[self._layout_override]
         return self.layouts[self.settings["layout"]]
+
+    def set_layout(self, layout: KeyboardLayout | None):
+        self._layout_override = layout
