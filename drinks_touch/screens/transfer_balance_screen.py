cea630be7d552daffca1cbfa7bb311f2b0b9e2a1
Soeren Wegener <wegener92@gmail.com>
2025-01-14 20:31:39 +0100

    Validate "transfer balance" input

--- drinks_touch/screens/transfer_balance_screen.py
+++ drinks_touch/screens/transfer_balance_screen.py
@@ -1,12 +1,13 @@
 import functools
 import logging
+from decimal import Decimal
 
 from sqlalchemy import func
 
 import config
 from database.models import Account
 from database.storage import Session
-from elements import Label
+from elements import Label, Button
 from elements.input_field import InputField, InputType
 from elements.spacer import Spacer
 from elements.vbox import VBox
@@ -40,6 +41,7 @@ def auto_complete_account_name(text, except_account: str, limit=10):
                 func.upper(func.substr(Account.name, n_char, 1)).label("n_char"),
                 func.count(Account.id),
             )
+            .filter(Account.name != except_account)
             .filter(Account.name.ilike(f"{text}%"))
             .group_by("n_char")
             .order_by("n_char")
@@ -59,6 +61,9 @@ class TransferBalanceScreen(Screen):
     def __init__(self, account):
         super().__init__()
         self.account = account
+        self.input_field_account_name = None
+        self.input_field_amount = None
+        self.label_error_message = None
 
     def on_start(self, *args, **kwargs):
         def focus(obj):
@@ -96,17 +101,53 @@ class TransferBalanceScreen(Screen):
                         height=50,
                     ),
                     Spacer(height=20),
-                    Label(
-                        text="Work in progress. Es fehlt:",
-                        size=15,
+                    label_error_message := Label(
+                        text="",
+                        size=20,
+                        color=config.Color.ERROR,
                     ),
-                    Label(
-                        text="- Nächster Screen zum bestätigen",
-                        size=15,
+                    Button(
+                        text="Übertragen",
+                        on_click=self.submit,
+                        padding=10,
                     ),
                 ],
                 pos=(5, 100),
             ),
         ]
 
+        self.input_field_account_name = input_field_account_name
+        self.input_field_amount = input_field_amount
+        self.label_error_message = label_error_message
+
         ScreenManager.instance.active_object = input_field_account_name
+
+    def submit(self):
+        account_name = self.input_field_account_name.text
+        amount = self.input_field_amount.text
+        if not account_name:
+            self.label_error_message.text = "Bitte gib einen Accountnamen ein."
+            return
+        if not amount:
+            self.label_error_message.text = "Bitte gib einen Betrag ein."
+            return
+
+        account = Account.query.filter(Account.name == account_name).first()
+        if not account:
+            self.label_error_message.text = "Account nicht gefunden."
+            return
+
+        try:
+            amount_decimal = Decimal(amount)
+        except ValueError as e:
+            logger.error(e)
+            self.label_error_message.text = str(e)
+            return
+
+        if amount_decimal <= 0:
+            self.label_error_message.text = "Der Betrag muss größer als 0 sein."
+            return
+
+        self.label_error_message.text = ""
+
+        # self.goto(...)
