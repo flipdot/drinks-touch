25fe6c316754d4495a2738be2e8d831f8a6d72bb
Soeren Wegener <wegener92@gmail.com>
2025-06-07 12:37:15 +0200

    Drop unused code from SvgIcon scaling
    Scaling now already when loading the image

--- drinks_touch/elements/icons/svg.py
+++ drinks_touch/elements/icons/svg.py
@@ -46,21 +46,10 @@ class SvgIcon(BaseIcon):
 
         img_width = image.get_width()
         img_height = image.get_height()
-        if width and height is None:
-            aspect_ratio = img_width / img_height
-            height = int(width / aspect_ratio)
-        elif height and width is None:
-            aspect_ratio = img_height / img_width
-            width = int(height / aspect_ratio)
 
-        if width and height:
-            self.image = pygame.transform.smoothscale(image, (width, height))
-            self.width = width + self.padding_left + self.padding_right
-            self.height = height + self.padding_top + self.padding_bottom
-        else:
-            self.image = image
-            self.width = img_width + self.padding_left + self.padding_right
-            self.height = img_height + self.padding_top + self.padding_bottom
+        self.image = image
+        self.width = img_width + self.padding_left + self.padding_right
+        self.height = img_height + self.padding_top + self.padding_bottom
 
         today = datetime.now().date()
         april_fools = today.month == 4 and today.day == 1
@@ -71,8 +60,12 @@ class SvgIcon(BaseIcon):
 
     @classmethod
     @functools.cache
-    def load_image(cls, path, color: Color | None = None, width=None, height=None):
-        png_bytes = cairosvg.svg2png(url=path, output_width=width, output_height=height)
+    def load_image(
+        cls, path: str | Path, color: Color | None = None, width=None, height=None
+    ):
+        png_bytes = cairosvg.svg2png(
+            url=str(path), output_width=width, output_height=height
+        )
         image = pygame.image.load(io.BytesIO(png_bytes)).convert_alpha()
         if color:
             image.fill(color.value, special_flags=pygame.BLEND_RGBA_MIN)
