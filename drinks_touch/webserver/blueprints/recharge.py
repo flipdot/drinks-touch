9892fb4ac343100e87db126bc20421ec27696751
Soeren Wegener <wegener92@gmail.com>
2025-09-06 01:12:31 +0200

    Dismissable alert boxes, better form validation

--- drinks_touch/webserver/blueprints/recharge.py
+++ drinks_touch/webserver/blueprints/recharge.py
@@ -1,16 +1,24 @@
+import decimal
 from decimal import Decimal
 
-from flask import Blueprint, render_template, g, url_for, flash
+from flask import Blueprint, render_template, g, url_for, flash, redirect
 from flask_wtf import FlaskForm
 from wtforms import validators
 from wtforms.fields.numeric import DecimalField
 from wtforms.fields.simple import BooleanField
+from wtforms.validators import InputRequired
 
 from webserver.shared import db, oidc
 
 from database.models import Tx
 
 bp = Blueprint("recharge", __name__)
+
+
+def validate_amount(form, field):
+    field.data = field.data.quantize(Decimal("0.01"), rounding=decimal.ROUND_DOWN)
+    if field.data == 0:
+        raise validators.ValidationError("Darf nicht 0 sein.")
 
 
 class RechargeForm(FlaskForm):
@@ -21,46 +29,38 @@ class RechargeForm(FlaskForm):
             "placeholder": "0.00",
             "step": "0.01",
         },
-        validators=[validators.Optional()],
+        validators=[InputRequired(message="Pflichtfeld"), validate_amount],
     )
     confirm_cash_deposited = BooleanField(
-        "Ja, ich habe den Betrag in die Kasse eingeworfen", default=False
+        "Ja, ich habe den Betrag in die Kasse eingeworfen",
+        default=False,
+        validators=[InputRequired(message="Pflichtfeld")],
     )
 
 
 @bp.route("/", methods=["GET", "POST"])
 @oidc.require_login
 def index():
-    form = RechargeForm(meta={"locales": ["de_DE", "de"]})
+    form = RechargeForm()
     if form.validate_on_submit():
         amount = form.data["amount"]
-        form_invalid = False
-        if not amount:
-            flash("Bitte einen Betrag angeben!", "error")
-            form_invalid = True
 
-        if not form.data["confirm_cash_deposited"]:
-            flash("Bitte bestätige, dass du das Geld eingeworfen hast!", "error")
-            form_invalid = True
+        tx = Tx(
+            payment_reference="Aufladung via Web",
+            account_id=g.account.id,
+            amount=amount,
+        )
+        db.session.add(tx)
+        db.session.commit()
 
-        if amount == "0":
-            flash("Ungültiger Betrag!", "error")
-            form_invalid = True
+        flash(f"Guthaben wurde um {amount:.2f}€ aufgeladen.", "success")
 
-        if not form_invalid:
-            tx = Tx(
-                payment_reference="Aufladung via Web",
-                account_id=g.account.id,
-                amount=Decimal(amount),
+        if amount < 0:
+            flash(
+                "Ein negativer Betrag… wusstest du, dass du auch Guthaben an andere übertragen kannst?",
+                "info",
             )
-            db.session.add(tx)
-            db.session.commit()
 
-            return render_template(
-                "recharge/success.html",
-                amount=amount,
-                account=g.account,
-                auto_redirect_after=5,
-                redirect_url=url_for("index"),
-            )
+        return redirect(url_for("index"))
+
     return render_template("recharge/index.html", form=form)
