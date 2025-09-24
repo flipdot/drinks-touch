8eb506a3790a217cb5c1b698cccc75bb9810b35b
Soeren Wegener <wegener92@gmail.com>
2025-09-25 01:32:37 +0200

    Styling of table and pagination

--- drinks_touch/webserver/blueprints/drinks.py
+++ drinks_touch/webserver/blueprints/drinks.py
@@ -9,5 +9,5 @@ bp = Blueprint("drinks", __name__)
 @bp.route("/pricelist")
 def pricelist():
     query = db.select(Drink).order_by(Drink.name)
-    drinks = db.paginate(query, per_page=5, max_per_page=100)
+    drinks = db.paginate(query, per_page=20, max_per_page=100)
     return render_template("drinks/pricelist.html", drinks=drinks)
