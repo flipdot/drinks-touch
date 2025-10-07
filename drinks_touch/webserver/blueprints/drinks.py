a361b6c5776721f36d691788b2e7d5b1de964bb2
Soeren Wegener <wegener92@gmail.com>
2025-10-07 23:11:32 +0200

    Sort drink to the top which have a price

--- drinks_touch/webserver/blueprints/drinks.py
+++ drinks_touch/webserver/blueprints/drinks.py
@@ -38,7 +38,7 @@ class EditDrinkForm(FlaskForm):
 
 @bp.route("/pricelist")
 def pricelist():
-    query = db.select(Drink).order_by(Drink.name)
+    query = db.select(Drink).order_by((Drink.price == None), Drink.name)  # noqa: E711
     drinks = db.paginate(query, per_page=20, max_per_page=100)
     return render_template("drinks/pricelist.html", drinks=drinks)
 
